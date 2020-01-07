8c3fd4aef79f06044621dd62e1ff89ba2e2fdea6
Håkon Dahle <hakon.dahle@gmail.com>
2020-01-07 06:10:09 +0100

    Added two charts. Added hack to move Latest Update to top of page

--- js/app.js
+++ js/app.js
@@ -20,8 +20,8 @@ Chart.defaults.global.plugins.colorschemes.scheme = 'brewer.SetTwo8';
 // 
 // Atmospheric CO2
 // 
-function plotAtmosphericCO2(elementIdA) {
-  var myChart2 = new Chart(document.getElementById(elementIdA), {
+function plotAtmosphericCO2(elementId) {
+  var myChart2 = new Chart(document.getElementById(elementId), {
     type: 'line',
     options: {
       legend: {
@@ -39,7 +39,6 @@ function plotAtmosphericCO2(elementIdA) {
       }
     }
   });
-
   fetch('https://probably.one:4438/co2')
     .then(status)
     .then(json)
@@ -54,6 +53,116 @@ function plotAtmosphericCO2(elementIdA) {
         label: 'Mauna Loa, Hawaii'
       });
       myChart2.update();
+    })
+    .catch(err => console.log(err));
+}
+
+//
+// Atmospheric CH4 Methane
+//
+function plotAtmosphericCH4(elementId) {
+  fetch('https://probably.one:4438/ch4')
+    .then(status)
+    .then(json)
+    .then(results => {
+      console.log('Atmospheric methane results:', results);
+      var myChart = new Chart(document.getElementById(elementId), {
+        type: 'line',
+        options: {
+          aspectRatio: 1,
+          plugins: {
+            colorschemes: {
+              scheme: 'tableau.Tableau10',
+              fillAlpha: 0.2
+            }
+          },
+          legend: {
+            display: false
+          },
+          tooltips: {
+            intersect: false,
+            mode: 'nearest'
+          },
+          scales: {
+            xAxes: [{
+              ticks: {
+                autoSkip: true,
+                maxTicksLimit: 8
+              },
+              type: 'time',
+            }]
+          }
+        }
+      });
+      let dates = results.data.map(x => x.date);
+      let values = results.data.map(x => x.average);
+      myChart.data.datasets.push({
+        label: "CH4 monthly",
+        data: values
+      });
+      myChart.data.labels = dates;
+      myChart.update();
+    })
+    .catch(err => console.log(err));
+}
+
+//
+// Arctic Ice Extent
+//
+function plotArcticIce(elementId) {
+  fetch('https://probably.one:4438/ice-nsidc')
+    .then(status)
+    .then(json)
+    .then(results => {
+      console.log('ICE NSIDC Results:', results);
+      var myChart = new Chart(document.getElementById(elementId), {
+        type: 'line',
+        options: {
+          aspectRatio: 1,
+          plugins: {
+            colorschemes: {
+              scheme: 'brewer.Spectral10'
+            }
+          },
+          responsive: true,
+          legend: {
+            display: true,
+            position: 'right',
+            align: 'middle',
+            reverse: true,
+            labels: {
+              boxWidth: 10,
+              padding: 4
+            },
+          },
+          tooltips: {
+            enabled: false
+          },
+          scales: {
+            xAxes: [{
+              gridLines: {
+                display: false
+              }
+            }],
+            yAxes: [{
+              stacked: false
+            }]
+          }
+        }
+      });
+      myChart.data.labels = ['J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'];
+      for (let year = 2019; year > 1978; year -= 5) {
+        // Extract a subset of data for a particular year
+        let tmp = results.data.filter(x => x.year === year);
+        // Then create a table that only contains the datapoints
+        let resval = tmp.map(x => x.extent);
+        myChart.data.datasets.push({
+          data: resval,
+          label: year,
+          fill: false
+        });
+        myChart.update();
+      }
     })
     .catch(err => console.log(err));
 }
